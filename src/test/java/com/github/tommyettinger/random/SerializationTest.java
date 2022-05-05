172914217448841bdce25a5a9f815d2b5a5b555e
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-05-04 20:25:40 -0700

    Reading doesn't work yet...

--- src/test/java/com/github/tommyettinger/random/SerializationTest.java
+++ src/test/java/com/github/tommyettinger/random/SerializationTest.java
@@ -1,6 +1,7 @@
 package com.github.tommyettinger.random;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SerializationTest {
@@ -16,6 +17,30 @@ public class SerializationTest {
             String s = r.stringSerialize();
             System.out.println(s);
             Assert.assertTrue(s.startsWith(r.getTag()));
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testRead() {
+        ChopRandom chop = new ChopRandom(-1L);
+        DistinctRandom distinct = new DistinctRandom(-1L);
+        MizuchiRandom mizuchi = new MizuchiRandom(-1L);
+        TricycleRandom tricycle = new TricycleRandom(-1L);
+        TrimRandom trim = new TrimRandom(-1L);
+        EnhancedRandom[] all = new EnhancedRandom[]{chop, distinct, mizuchi, tricycle, trim};
+        String[] serialized = new String[]{
+                "ChpR`FFFFFFFFC9E6AE5E~FFFFFFFFE546FE33~FFFFFFFFD27DC844~FFFFFFFFC8C3F0AA`",
+                "DisR`FFFFFFFFFFFFFFFF`",
+                "MizR`087164D5C9E6AE5E~67B862BCE546FE33`",
+                "TriR`087164D5C9E6AE5E~67B862BCE546FE33~329AE2C1D27DC844`",
+                "TrmR`507BEC21852B4AD5~69383448E8617609~AF8413DE7AD4B52A~96C7CBB7179E89F6`",
+        };
+        int idx = 0;
+        for(String s : serialized){
+            EnhancedRandom x = all[idx++], r = Deserializer.deserialize(s);
+            System.out.println(x + "  ,  " + r);
+            Assert.assertTrue(EnhancedRandom.areEqual(r, x));
         }
     }
 }
