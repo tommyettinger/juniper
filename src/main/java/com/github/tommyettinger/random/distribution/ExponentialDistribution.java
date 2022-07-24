819b8c0604c3d4ae84501f7bbf827e7fd6ffedf3
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-07-23 21:25:36 -0700

    Pareto!

--- src/main/java/com/github/tommyettinger/random/distribution/ExponentialDistribution.java
+++ src/main/java/com/github/tommyettinger/random/distribution/ExponentialDistribution.java
@@ -1,6 +1,5 @@
 package com.github.tommyettinger.random.distribution;
 
-import com.github.tommyettinger.digital.MathTools;
 import com.github.tommyettinger.random.EnhancedRandom;
 import com.github.tommyettinger.random.WhiskerRandom;
 
--- src/main/java/com/github/tommyettinger/random/distribution/ParetoDistribution.java
+++ src/main/java/com/github/tommyettinger/random/distribution/ParetoDistribution.java
@@ -6,9 +6,9 @@ import com.github.tommyettinger.random.WhiskerRandom;
 
 /**
  * A two-parameter distribution with range from 0 to positive infinity.
- * @see <a href="https://en.wikipedia.org/wiki/Beta_prime_distribution">Wikipedia's page on this distribution.</a>
+ * @see <a href="https://en.wikipedia.org/wiki/Pareto_distribution">Wikipedia's page on this distribution.</a>
  */
-public class BetaPrimeDistribution extends Distribution {
+public class ParetoDistribution extends Distribution {
     private double alpha;
     private double beta;
 
@@ -21,23 +21,23 @@ public class BetaPrimeDistribution extends Distribution {
     }
 
     /**
-     * Uses a {@link WhiskerRandom}, alpha = 2.0, beta = 2.0 .
+     * Uses a {@link WhiskerRandom}, alpha = 1.0, beta = 1.0 .
      */
-    public BetaPrimeDistribution() {
-        this(new WhiskerRandom(), 2.0, 2.0);
+    public ParetoDistribution() {
+        this(new WhiskerRandom(), 1.0, 1.0);
     }
 
     /**
      * Uses a {@link WhiskerRandom} and the given alpha and beta.
      */
-    public BetaPrimeDistribution(double alpha, double beta) {
+    public ParetoDistribution(double alpha, double beta) {
         this(new WhiskerRandom(), alpha, beta);
     }
 
     /**
      * Uses the given EnhancedRandom directly. Uses the given alpha and beta.
      */
-    public BetaPrimeDistribution(EnhancedRandom generator, double alpha, double beta)
+    public ParetoDistribution(EnhancedRandom generator, double alpha, double beta)
     {
         this.generator = generator;
         if(!setParameters(alpha, beta, 0.0))
@@ -51,41 +51,43 @@ public class BetaPrimeDistribution extends Distribution {
 
     @Override
     public double getMean() {
-        return alpha / (beta - 1.0);
+        if (beta > 1.0)
+            return alpha * beta / (beta - 1.0);
+        throw new UnsupportedOperationException("Mean cannot be determined for the given parameters.");
     }
 
     @Override
     public double getMedian() {
-        throw new UnsupportedOperationException("Median is undefined.");
+        return alpha * Math.pow(2.0, 1.0 / beta);
     }
 
     @Override
     public double getMinimum() {
-        return 0.0;
+        return alpha;
     }
 
     @Override
     public double[] getMode() {
-        return new double[]{(alpha - 1.0) / (beta + 1.0)};
+        return new double[]{ alpha };
     }
 
     @Override
     public double getVariance() {
         if(beta > 2.0)
-            return alpha * (alpha + beta - 1.0) / MathTools.square(beta - 1.0) * (beta - 2.0);
+            return beta * alpha * alpha / MathTools.square(beta - 1.0) * (beta - 2.0);
         throw new UnsupportedOperationException("Variance cannot be determined for the given parameters.");
     }
 
     /**
      * Sets all parameters and returns true if they are valid, otherwise leaves parameters unchanged and returns false.
-     * @param a alpha; should be greater than 1.0
-     * @param b beta; should be greater than 1.0
+     * @param a alpha; should be greater than 0.0
+     * @param b beta; should be greater than 0.0
      * @param c ignored
      * @return true if the parameters given are valid and will be used
      */
     @Override
     public boolean setParameters(double a, double b, double c) {
-        if(a > 1.0 && b > 1.0){
+        if(a > 0.0 && b > 0.0){
             alpha = a;
             beta = b;
             return true;
@@ -99,9 +101,6 @@ public class BetaPrimeDistribution extends Distribution {
     }
 
     public static double sample(EnhancedRandom generator, double alpha, double beta) {
-        double variate = BetaDistribution.sample(generator, alpha, beta);
-        double rev = 1.0 - variate;
-        return MathTools.isZero(rev, 0x1p-32) ? Double.POSITIVE_INFINITY : variate / rev;
-
+        return alpha * Math.exp(ExponentialDistribution.sample(generator, beta));
     }
 }
