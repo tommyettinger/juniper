9d1de64f877e1b5523f41bd2a344bc4ab9b3f5dd
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-07-27 21:20:12 -0700

    Discrete Uniform, adjust Continuous too.

--- src/main/java/com/github/tommyettinger/random/distribution/DiscreteUniformDistribution.java
+++ src/main/java/com/github/tommyettinger/random/distribution/DiscreteUniformDistribution.java
@@ -5,39 +5,39 @@ import com.github.tommyettinger.random.EnhancedRandom;
 import com.github.tommyettinger.random.WhiskerRandom;
 
 /**
- * A two-parameter distribution with range between the given parameters, both inclusive.
- * @see <a href="https://en.wikipedia.org/wiki/Continuous_uniform_distribution">Wikipedia's page on this distribution.</a>
+ * A two-parameter discrete distribution with integer range between the given parameters, both inclusive.
+ * @see <a href="http://en.wikipedia.org/wiki/Uniform_distribution_%28discrete%29">Wikipedia's page on this distribution.</a>
  */
-public class ContinuousUniformDistribution extends Distribution {
-    private double alpha;
-    private double beta;
+public class DiscreteUniformDistribution extends Distribution {
+    private int alpha;
+    private int beta;
 
-    public double getAlpha() {
+    public int getAlpha() {
         return alpha;
     }
 
-    public double getBeta() {
+    public int getBeta() {
         return beta;
     }
 
     /**
-     * Uses a {@link WhiskerRandom}, alpha = 0.0, beta = 1.0 .
+     * Uses a {@link WhiskerRandom}, alpha = 0, beta = 1 .
      */
-    public ContinuousUniformDistribution() {
-        this(new WhiskerRandom(), 0.0, 1.0);
+    public DiscreteUniformDistribution() {
+        this(new WhiskerRandom(), 0, 1);
     }
 
     /**
      * Uses a {@link WhiskerRandom} and the given alpha and beta.
      */
-    public ContinuousUniformDistribution(double alpha, double beta) {
+    public DiscreteUniformDistribution(double alpha, double beta) {
         this(new WhiskerRandom(), alpha, beta);
     }
 
     /**
      * Uses the given EnhancedRandom directly. Uses the given alpha and beta.
      */
-    public ContinuousUniformDistribution(EnhancedRandom generator, double alpha, double beta)
+    public DiscreteUniformDistribution(EnhancedRandom generator, double alpha, double beta)
     {
         this.generator = generator;
         if(!setParameters(alpha, beta, 0.0))
@@ -71,20 +71,20 @@ public class ContinuousUniformDistribution extends Distribution {
 
     @Override
     public double getVariance() {
-        return MathTools.square(beta - alpha) / 12.0;
+        return (MathTools.square(beta - alpha + 1.0) - 1.0) / 12.0;
     }
     /**
      * Sets all parameters and returns true if they are valid, otherwise leaves parameters unchanged and returns false.
-     * @param a alpha; should be less than or equal to beta
-     * @param b beta; should be greater than or equal to alpha
+     * @param a alpha; will be cast to int, and should be less than or equal to beta
+     * @param b beta; will be cast to int, and should be greater than or equal to alpha
      * @param c ignored
      * @return true if the parameters given are valid and will be used
      */
     @Override
     public boolean setParameters(double a, double b, double c) {
-        if(a <= b){
-            alpha = a;
-            beta = b;
+        if((int)a <= (int)b){
+            alpha = (int)a;
+            beta = (int)b;
             return true;
         }
         return false;
@@ -95,7 +95,7 @@ public class ContinuousUniformDistribution extends Distribution {
         return sample(generator, alpha, beta);
     }
 
-    public static double sample(EnhancedRandom generator, double alpha, double beta) {
-        return generator.nextInclusiveDouble(alpha, beta);
+    public static double sample(EnhancedRandom generator, int alpha, int beta) {
+        return generator.nextLong(alpha, beta+1L);
     }
 }
