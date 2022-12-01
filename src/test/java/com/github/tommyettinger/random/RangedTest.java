95b1a9b99b7f3d4be72af6e1f1370cec2e47f623
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-11-30 21:32:23 -0800

    Test many more limits, slowly easing threshold.
    The threshold won't go lower than 0.8666666666666667, which isn't much different from the 0.9 before, but does matter in some cases.

--- src/test/java/com/github/tommyettinger/random/RangedTest.java
+++ src/test/java/com/github/tommyettinger/random/RangedTest.java
@@ -8,20 +8,27 @@ import java.util.ArrayList;
 public class RangedTest {
     @Test
     public void testIntRange() {
-        final int limit = 11;
-        ArrayList<EnhancedRandom> randoms = Deserializer.copyRandoms();
-        for(EnhancedRandom r : randoms) {
-            int[] buckets = new int[limit];
-            for (int i = limit << 20; i > 0; i--) {
-                buckets[r.nextInt(limit)]++;
+        for (int limit = 2; limit < 102; limit++) {
+            double threshold = Math.pow(3.0, limit);
+            threshold = 1.2-(threshold-1.0)/(3.0*(threshold+1.0));
+            System.out.println("Testing all EnhancedRandom using nextInt("+limit+") with threshold " + threshold);
+            ArrayList<EnhancedRandom> randoms = Deserializer.copyRandoms();
+            for (EnhancedRandom r : randoms) {
+                int[] buckets = new int[limit];
+                for (int i = limit << 12; i > 0; i--) {
+                    buckets[r.nextInt(limit)]++;
+                }
+                int min = Integer.MAX_VALUE, max = -1;
+                for (int i = 0; i < limit; i++) {
+                    min = Math.min(min, buckets[i]);
+                    max = Math.max(max, buckets[i]);
+                }
+                Assert.assertTrue("Distribution failure for nextInt() with limit " + limit + " using " + r + ": " +
+                                ((double) min / (double) max),
+                        (double) min / (double) max >= threshold);
+//                System.out.println("Distribution success for nextInt() with limit " + limit + " using " + r.getTag() + ": " +
+//                        ((double) min / (double) max));
             }
-            int min = Integer.MAX_VALUE, max = -1;
-            for (int i = 0; i < limit; i++) {
-                min = Math.min(min, buckets[i]);
-                max = Math.max(max, buckets[i]);
-            }
-            Assert.assertTrue("Distribution failure for nextInt() with limit " + limit + " using " + r,
-                    (double)min / (double)max >= 0.9);
         }
     }
 }
