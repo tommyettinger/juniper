7ed4e942572cf2dffbb61de5c0cb70d69877275e
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-11-30 21:58:53 -0800

    Try testing with many more limits.

--- src/test/java/com/github/tommyettinger/random/RangedTest.java
+++ src/test/java/com/github/tommyettinger/random/RangedTest.java
@@ -1,21 +1,26 @@
 package com.github.tommyettinger.random;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 public class RangedTest {
+    @Ignore // comment this out if you want to run this; it can take a little while
     @Test
     public void testIntRange() {
-        for (int limit = 2; limit < 102; limit++) {
-            double threshold = Math.pow(3.0, limit);
-            threshold = 1.2-(threshold-1.0)/(3.0*(threshold+1.0));
+        for (int limit = 2; limit <= 1024; limit++) {
+//            double threshold = Math.pow(3.0, limit);
+//            threshold = 1.2-(threshold-1.0)/(2.5*(threshold+1.0));
+            // we need to use tanh because manually calculating can give very wrong results for high limits.
+            double threshold = 1.91 - Math.tanh(limit);
+            threshold *= threshold;
             System.out.println("Testing all EnhancedRandom using nextInt("+limit+") with threshold " + threshold);
             ArrayList<EnhancedRandom> randoms = Deserializer.copyRandoms();
             for (EnhancedRandom r : randoms) {
                 int[] buckets = new int[limit];
-                for (int i = limit << 12; i > 0; i--) {
+                for (int i = limit << 11; i > 0; i--) {
                     buckets[r.nextInt(limit)]++;
                 }
                 int min = Integer.MAX_VALUE, max = -1;
