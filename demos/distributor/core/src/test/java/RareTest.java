7029b95b3ba53bae8c0f8fdfa08db7cea9bf5638
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-12-10 20:25:43 -0800

    Test rare doubles.

--- demos/distributor/core/src/test/java/RareTest.java
+++ demos/distributor/core/src/test/java/RareTest.java
@@ -60,4 +60,56 @@ public class RareTest {
         }
         System.out.println("With seed " + SEED + " on RandomXS128.nextFloat(), the count is " + sum + " and should be roughly 1000 .");
     }
+    /**
+     * With WhiskerRandom.nextDouble(), the count is 1029 and should be roughly 1000 .
+     */
+    @Test
+    public void nextDoubleWhisker() {
+        WhiskerRandom random = new WhiskerRandom(SEED);
+        int sum = 0;
+        for (int i = 0; i < 1000000000; i++) {
+            if(random.nextDouble() < 1e-6f)
+                ++sum;
+        }
+        System.out.println("With seed " + SEED + " on WhiskerRandom.nextDouble(), the count is " + sum + " and should be roughly 1000 .");
+    }
+    /**
+     * With DistinctRandom.nextDouble(), the count is 1015 and should be roughly 1000 .
+     */
+    @Test
+    public void nextDoubleDistinct() {
+        DistinctRandom random = new DistinctRandom(SEED);
+        int sum = 0;
+        for (int i = 0; i < 1000000000; i++) {
+            if(random.nextDouble() < 1e-6f)
+                ++sum;
+        }
+        System.out.println("With seed " + SEED + " on DistinctRandom.nextDouble(), the count is " + sum + " and should be roughly 1000 .");
+    }
+    /**
+     * With Random.nextDouble(), the count is 1014 and should be roughly 1000 .
+     */
+    @Test
+    public void nextDoubleJDK() {
+        Random random = new Random(SEED);
+        int sum = 0;
+        for (int i = 0; i < 1000000000; i++) {
+            if(random.nextDouble() < 1e-6f)
+                ++sum;
+        }
+        System.out.println("With seed " + SEED + " on Random.nextDouble(), the count is " + sum + " and should be roughly 1000 .");
+    }
+    /**
+     * With RandomXS128.nextDouble(), the count is 1012 and should be roughly 1000 .
+     */
+    @Test
+    public void nextDoubleGDX() {
+        RandomXS128 random = new RandomXS128(SEED);
+        int sum = 0;
+        for (int i = 0; i < 1000000000; i++) {
+            if(random.nextDouble() < 1e-6f)
+                ++sum;
+        }
+        System.out.println("With seed " + SEED + " on RandomXS128.nextDouble(), the count is " + sum + " and should be roughly 1000 .");
+    }
 }
