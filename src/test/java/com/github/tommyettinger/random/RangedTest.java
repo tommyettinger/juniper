9af7c8fed1d61ce867d3c16fde4dee1265138c86
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-12-15 20:25:30 -0800

    RangedTest should also test longs.
    We're limited to testing relatively small longs, though (from 2 up to 1024).

--- src/test/java/com/github/tommyettinger/random/RangedTest.java
+++ src/test/java/com/github/tommyettinger/random/RangedTest.java
@@ -11,8 +11,6 @@ public class RangedTest {
     @Test
     public void testIntRange() {
         for (int limit = 2; limit <= 1024; limit++) {
-//            double threshold = Math.pow(3.0, limit);
-//            threshold = 1.2-(threshold-1.0)/(2.5*(threshold+1.0));
             // we need to use tanh because manually calculating can give very wrong results for high limits.
             double threshold = 1.91 - Math.tanh(limit);
             threshold *= threshold;
@@ -32,6 +30,33 @@ public class RangedTest {
                                 ((double) min / (double) max),
                         (double) min / (double) max >= threshold);
 //                System.out.println("Success for nextInt() with limit " + limit + " using " + r.getTag() + ": " +
+//                        ((double) min / (double) max));
+            }
+        }
+    }
+    @Ignore // comment this out if you want to run this; it can take a little while
+    @Test
+    public void testLongRange() {
+        for (int limit = 2; limit <= 1024; limit++) {
+            // we need to use tanh because manually calculating can give very wrong results for high limits.
+            double threshold = 1.91 - Math.tanh(limit);
+            threshold *= threshold;
+            System.out.println("Testing all EnhancedRandom using nextLong("+limit+") with threshold " + threshold);
+            ArrayList<EnhancedRandom> randoms = Deserializer.copyRandoms();
+            for (EnhancedRandom r : randoms) {
+                int[] buckets = new int[limit];
+                for (int i = limit << 11; i > 0; i--) {
+                    buckets[(int)r.nextLong(limit)]++;
+                }
+                int min = Integer.MAX_VALUE, max = -1;
+                for (int i = 0; i < limit; i++) {
+                    min = Math.min(min, buckets[i]);
+                    max = Math.max(max, buckets[i]);
+                }
+                Assert.assertTrue("Distribution failure for nextLong() with limit " + limit + " using " + r + ": " +
+                                ((double) min / (double) max),
+                        (double) min / (double) max >= threshold);
+//                System.out.println("Success for nextLong() with limit " + limit + " using " + r + ": " +
 //                        ((double) min / (double) max));
             }
         }
