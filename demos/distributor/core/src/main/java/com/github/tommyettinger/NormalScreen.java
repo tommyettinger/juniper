e5a3e02d607df0362d1527a43aae0e3fd0be5ed8
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-02-15 15:32:09 -0800

    Update to Gradle 8.0, compat fixes.
    This also rebuilds distributor in the docs/ folder.

--- demos/distributor/core/src/main/java/com/github/tommyettinger/NormalScreen.java
+++ demos/distributor/core/src/main/java/com/github/tommyettinger/NormalScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Camera;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -13,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.UIUtils;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.github.tommyettinger.random.distribution.CauchyDistribution;
 import com.github.tommyettinger.random.distribution.NormalDistribution;
 import text.formic.Stringf;
 
