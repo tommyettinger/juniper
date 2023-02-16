57ed099f3aefb946137fb7763c12fb20ae277ad2
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-02-15 20:27:12 -0800

    Copyright and package-info stuff.

---
+++ src/main/java/com/github/tommyettinger/random/distribution/package-info.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * Statistical distributions that make use of {@link com.github.tommyettinger.random.EnhancedRandom EnhancedRandom}
+ * generators from this library.
+ * <br>
+ * Most of the generators here are adapted from <a href="https://github.com/tommyettinger/ShaiRandom">ShaiRandom</a>,
+ * which got much of its code from <a href="https://gitlab.com/pomma89/troschuetz-random/">Troschuetz.Random</a>, but
+ * took special care to avoid GPL- or LGPL-licensed code (and either removed or rewrote that code).
+ */
+package com.github.tommyettinger.random.distribution;
