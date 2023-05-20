9ff5659b32770859326afe5994e5c1fb03dbd088
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-05-19 23:08:10 -0700

    Add a seeding test; setSeed() should be strong...
    But, it isn't for ScruffRandom or, sort-of, AceRandom. Of course various quasi-random generators don't produce very good early output, since this is often by design.

---
+++ src/test/java/com/github/tommyettinger/random/SeedingTest.java
@@ -0,0 +1,29 @@
+package com.github.tommyettinger.random;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class SeedingTest {
+    @Test
+    public void testSmallSeeds() {
+        long[] seeds = new long[]{0L, 1L, 2L, 3L, 4L};
+        ArrayList<EnhancedRandom> randoms = Deserializer.copyRandoms();
+        for(EnhancedRandom rng : randoms){
+            for(long seed : seeds){
+                rng.setSeed(seed);
+                long earlier = rng.nextLong();
+                long later;
+                int count = 32;
+                for (int i = 1; i < 10; i++) {
+                    later = rng.nextLong();
+                    if((count = Long.bitCount(earlier ^ later)) < 8)
+                        System.out.println(rng + " with initial seed " + seed + " had a difference of " + count + " bits on generated numbers "
+                                + (i - 1) + " and " + i);
+                    earlier = later;
+                }
+
+            }
+        }
+    }
+}
