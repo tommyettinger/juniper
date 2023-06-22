045b5c0693021c294874fdc61afa330f4870b5fd
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-06-21 19:50:04 -0700

    Test correlation between similar initial states.
    This brings over a test from SquidSquad, since it belongs better here, and makes the tests depend on libGDX so this can be visually previewed. That turns out to be a big help when noticing patterns.

---
+++ src/test/java/com/github/tommyettinger/random/CorrelationVisualizer.java
@@ -0,0 +1,264 @@
+/*
+ * Copyright (c) 2022-2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tommyettinger.random;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer20;
+import com.badlogic.gdx.scenes.scene2d.utils.UIUtils;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.github.tommyettinger.digital.BitConversion;
+import com.github.tommyettinger.random.experimental.*;
+
+import static com.badlogic.gdx.Input.Keys.*;
+import static com.badlogic.gdx.graphics.GL20.GL_POINTS;
+
+/**
+ */
+public class CorrelationVisualizer extends ApplicationAdapter {
+
+    private String title = "";
+    private ImmediateModeRenderer20 renderer;
+    private static final int width = 256, height = 256;
+    private static final float[][] previousGrid = new float[width][height];
+    public static EnhancedRandom[][] makeGrid(EnhancedRandom base){
+        EnhancedRandom[][] g = new EnhancedRandom[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                g[x][y] = base.copy();
+                switch (g[x][y].getStateCount()) {
+                    case 1:
+                        g[x][y].setState(x << 17 ^ y << 1);
+                        break;
+                    case 2:
+                        g[x][y].setState(x << 1, y << 1);
+                        break;
+                    case 3:
+                        g[x][y].setState(x << 1, y << 1, 1L);
+                        break;
+                    case 4:
+                        g[x][y].setState(x << 1, y << 1, 1L, 1L);
+                        break;
+                    case 5:
+                        g[x][y].setState(x << 1, y << 1, 1L, 1L, 1L);
+                        break;
+                }
+            }
+        }
+        return g;
+    }
+    private static final EnhancedRandom[][][] randoms = new EnhancedRandom[][][]{
+            makeGrid(new LaserRandom(1, 1)),
+            makeGrid(new MizuchiRandom(1, 1)),
+            makeGrid(new TricycleRandom(1, 1, 1)),
+            makeGrid(new FourWheelRandom(1, 1, 1, 1)),
+            makeGrid(new TrimRandom(1, 1, 1, 1)),
+            makeGrid(new WhiskerRandom(1, 1, 1, 1)),
+            makeGrid(new ScruffRandom(1, 1, 1, 1)),
+            makeGrid(new ScarfRandom(1, 1, 1, 1)),
+            makeGrid(new PasarRandom(1, 1, 1, 1, 1)),
+            makeGrid(new AceRandom(1, 1, 1, 1, 1)),
+            makeGrid(new SplurgeRandom(1, 1)),
+            makeGrid(new SportyRandom(1, 1)),
+            makeGrid(new SpoonRandom(1, 1)),
+            makeGrid(new SpritzRandom(1, 1)),
+    };
+    int currentRandom = 0, randomCount = randoms.length;
+    int currentMode = 0, modeCount = 3;
+
+    public static void refreshGrid() {
+        for (int i = 0, n = randoms.length; i < n; i++) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    switch (randoms[i][x][y].getStateCount()) {
+                        case 1:
+                            randoms[i][x][y].setState(x << 17 ^ y << 1);
+                            break;
+                        case 2:
+                            randoms[i][x][y].setState(x << 1, y << 1);
+                            break;
+                        case 3:
+                            randoms[i][x][y].setState(x << 1, y << 1, 1L);
+                            break;
+                        case 4:
+                            randoms[i][x][y].setState(x << 1, y << 1, 1L, 1L);
+                            break;
+                        case 5:
+                            randoms[i][x][y].setState(x << 1, y << 1, 1L, 1L, 1L);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void seedGrid() {
+        for (int i = 0, n = randoms.length; i < n; i++) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    randoms[i][x][y].setSeed(x << 17 ^ y << 1);
+                }
+            }
+        }
+    }
+
+    private Viewport view;
+    private boolean keepGoing = true;
+
+    @Override
+    public void create() {
+        title = randoms[currentRandom][0][0].getClass().getSimpleName() + " on mode " + currentMode;
+        System.out.println(title);
+        renderer = new ImmediateModeRenderer20(width * height, false, true, 0);
+        view = new ScreenViewport();
+        InputAdapter input = new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+                switch (keycode) {
+                    case SPACE:
+                    case P: // pause
+                        keepGoing = !keepGoing;
+                        break;
+                    case S: // step
+                        putMap();
+                        break;
+                    case V: // vertical
+                        System.out.println("Viewing column 0:");
+                        System.out.println(randoms[currentRandom][0][0]);
+                        System.out.println(randoms[currentRandom][0][1]);
+                        System.out.println(randoms[currentRandom][0][2]);
+                        System.out.println(randoms[currentRandom][0][3]);
+                        break;
+                    case H: // horizontal
+                        System.out.println("Viewing row 0:");
+                        System.out.println(randoms[currentRandom][0][0]);
+                        System.out.println(randoms[currentRandom][1][0]);
+                        System.out.println(randoms[currentRandom][2][0]);
+                        System.out.println(randoms[currentRandom][3][0]);
+                        break;
+                    case NUM_1:
+                    case NUMPAD_1:
+                        seedGrid();
+                        putMap();
+                        break;
+                    case N: // next
+                    case EQUALS:
+                    case ENTER:
+                        currentRandom = ((currentRandom + (UIUtils.shift() ? randomCount - 1 : 1)) % randomCount);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " on mode " + currentMode;
+                        System.out.println(title);
+                        if (!keepGoing) putMap();
+                        break;
+                    case M: // mode
+                        currentMode = ((currentMode + (UIUtils.shift() ? modeCount - 1 : 1)) % modeCount);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " on mode " + currentMode;
+                        System.out.println(title);
+                        if (!keepGoing) putMap();
+                        break;
+                    case Q: // quit
+                    case ESCAPE: {
+                        Gdx.app.exit();
+                    }
+                }
+                return true;
+            }
+        };
+        Gdx.input.setInputProcessor(input);
+    }
+
+    public void putMap() {
+        renderer.begin(view.getCamera().combined, GL_POINTS);
+        int bt;
+        switch (currentMode) {
+            case 0:
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        bt = (int) randoms[currentRandom][x][y].nextLong() & 255;
+                        renderer.color(previousGrid[x][y] = BitConversion.intBitsToFloat(0xFE000000 | bt << 16 | bt << 8 | bt));
+                        renderer.vertex(x, y, 0);
+                    }
+                }
+                break;
+            case 1:
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        bt = -((int) randoms[currentRandom][x][y].nextLong() & 1) >>> 8;
+                        renderer.color(previousGrid[x][y] = BitConversion.intBitsToFloat(0xFE000000 | bt));
+                        renderer.vertex(x, y, 0);
+                    }
+                }
+                break;
+            case 2:
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        bt = -(int) (randoms[currentRandom][x][y].nextLong() >>> 63) >>> 8;
+                        renderer.color(previousGrid[x][y] = BitConversion.intBitsToFloat(0xFE000000 | bt));
+                        renderer.vertex(x, y, 0);
+                    }
+                }
+                break;
+        }
+        renderer.end();
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.BLACK);
+        Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() + " FPS showing " + title);
+        if (keepGoing) {
+            putMap();
+        }
+        else {
+            renderer.begin(view.getCamera().combined, GL_POINTS);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    renderer.color(previousGrid[x][y]);
+                    renderer.vertex(x, y, 0);
+                }
+            }
+            renderer.end();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        view.update(width, height, true);
+        view.apply(true);
+    }
+
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.useVsync(true);
+        config.setForegroundFPS(0);
+        config.setResizable(false);
+        config.setWindowedMode(width, height);
+        config.disableAudio(true);
+        new Lwjgl3Application(new CorrelationVisualizer(), config);
+    }
+}
---
+++ src/test/java/com/github/tommyettinger/random/experimental/SpoonRandom.java
@@ -0,0 +1,337 @@
+/*
+ * Copyright (c) 2022 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.random.experimental;
+
+import com.github.tommyettinger.random.EnhancedRandom;
+
+/**
+ * 190 bits of state. Period is 2 to the 64.
+ * There are 2 to the 126 possible streams.
+ * Both streamB and streamC are always odd numbers,
+ * which is why this has a slightly smaller state size.
+ * <br>
+ * I wanna change
+ * your mind
+ * Said I wanna set it right
+ * this time
+ * I'm looking through you
+ * You know who you are
+ */
+public class SpoonRandom extends EnhancedRandom {
+	@Override
+	public String getTag() {
+		return "SpnR";
+	}
+
+	/**
+	 * The first state; can be any long.
+	 */
+	protected long stateA;
+	/**
+	 * The second state; can be any odd long.
+	 */
+	protected long stateB;
+	/**
+	 * The third state; can be any odd long.
+	 */
+	protected long stateC;
+
+	/**
+	 * Creates a new SpoonRandom with a random state.
+	 */
+	public SpoonRandom() {
+		stateA = EnhancedRandom.seedFromMath();
+		stateB = EnhancedRandom.seedFromMath() | 1L;
+		stateC = EnhancedRandom.seedFromMath() | 1L;
+	}
+
+	/**
+	 * Creates a new SpoonRandom with the given seed; all {@code long} values are permitted.
+	 * The seed will be passed to {@link #setSeed(long)} to attempt to adequately distribute the seed randomly.
+	 *
+	 * @param seed any {@code long} value
+	 */
+	public SpoonRandom(long seed) {
+		setSeed(seed);
+	}
+
+	/**
+	 * Creates a new SpoonRandom with the given two states; all {@code long} values are permitted.
+	 * These states will be used verbatim for stateA and stateB. stateC will be assigned 1.
+	 *
+	 * @param stateA any {@code long} value
+	 * @param stateB any odd {@code long} value (if even, will be made odd)
+	 */
+	public SpoonRandom(long stateA, long stateB) {
+		this.stateA = stateA;
+		this.stateB = stateB | 1L;
+		this.stateC = 1L;
+	}
+
+	/**
+	 * Creates a new SpoonRandom with the given four states; all {@code long} values are permitted.
+	 * These states will be used verbatim.
+	 *
+	 * @param stateA any {@code long} value
+	 * @param stateB any odd {@code long} value (if even, will be made odd)
+	 * @param stateC any odd {@code long} value (if even, will be made odd)
+	 */
+	public SpoonRandom(long stateA, long stateB, long stateC) {
+		this.stateA = stateA;
+		this.stateB = stateB | 1L;
+		this.stateC = stateC | 1L;
+	}
+
+	/**
+	 * This generator has 3 {@code long} states, so this returns 3.
+	 *
+	 * @return 3 (three)
+	 */
+	@Override
+	public int getStateCount () {
+		return 3;
+	}
+
+	/**
+	 * Gets the state determined by {@code selection}, as-is. The value for selection should be
+	 * between 0 and 2, inclusive; if it is any other value this gets state C as if 2 was given.
+	 *
+	 * @param selection used to select which state variable to get; generally 0, 1, or 2
+	 * @return the value of the selected state
+	 */
+	@Override
+	public long getSelectedState (int selection) {
+		switch (selection) {
+			case 0:
+				return stateA;
+			case 1:
+				return stateB;
+			default:
+				return stateC;
+		}
+	}
+
+	/**
+	 * Sets one of the states, determined by {@code selection}, to {@code value}, as-is.
+	 * Selections 0, 1, and 2 refer to states A, B, and C, and if the selection is anything
+	 * else, this ignores it and sets nothing. States B and C are always odd, so if they would
+	 * be set with an even number this adds 1 to value before setting.
+	 *
+	 * @param selection used to select which state variable to set; generally 0, 1, or 2
+	 * @param value     the exact value to use for the selected state, if valid
+	 */
+	@Override
+	public void setSelectedState (int selection, long value) {
+		switch (selection) {
+		case 0:
+			stateA = value;
+			break;
+		case 1:
+			stateB = value | 1L;
+			break;
+		case 2:
+			stateC = value | 1L;
+			break;
+		}
+	}
+
+	/**
+	 * This initializes all 4 states of the generator to random values based on the given seed.
+	 * (2 to the 64) possible initial generator states can be produced here, though there are
+	 * (2 to the 190) possible states in total.
+	 *
+	 * @param seed the initial seed; may be any long
+	 */
+	@Override
+	public void setSeed (long seed) {
+		stateA = seed;
+		seed ^= seed >>> 32;
+		seed *= 0xbea225f9eb34556dL;
+		seed ^= seed >>> 29;
+		seed *= 0xbea225f9eb34556dL;
+		seed ^= seed >>> 32;
+		seed *= 0xbea225f9eb34556dL;
+		seed ^= seed >>> 29;
+		stateB = (seed ^ 0xC6BC279692B5C323L) | 1L;
+		stateC = (seed ^ ~0xC6BC279692B5C323L) | 1L;
+	}
+
+	public long getStateA () {
+		return stateA;
+	}
+
+	/**
+	 * Sets the first part of the state.
+	 *
+	 * @param stateA can be any long
+	 */
+	public void setStateA (long stateA) {
+		this.stateA = stateA;
+	}
+
+	public long getStateB () {
+		return stateB;
+	}
+
+	/**
+	 * Sets the second part of the state.
+	 *
+	 * @param stateB can be any odd long (if even, will be made odd)
+	 */
+	public void setStateB (long stateB) {
+		this.stateB = stateB | 1L;
+	}
+
+	public long getStateC () {
+		return stateC;
+	}
+
+	/**
+	 * Sets the third part of the state.
+	 *
+	 * @param stateC can be any odd long (if even, will be made odd)
+	 */
+	public void setStateC (long stateC) {
+		this.stateC = stateC | 1L;
+	}
+
+	/**
+	 * Equivalent to {@code setState(stateA, stateB, 1L)}.
+	 *
+	 * @param stateA the long value to use for stateA
+	 * @param stateB the odd long value to use for stateB (if even, will be made odd)
+	 */
+	@Override
+	public void setState(long stateA, long stateB) {
+		setState(stateA, stateB, 1L);
+	}
+
+	/**
+	 * Sets the state completely to the given four state variables.
+	 * This is the same as calling {@link #setStateA(long)}, {@link #setStateB(long)},
+	 * and {@link #setStateC(long)} as a group.
+	 *
+	 * @param stateA the first state; can be any long
+	 * @param stateB the second state; can be any odd long (if even, will be made odd)
+	 * @param stateC the third state; can be any odd long (if even, will be made odd)
+	 */
+	@Override
+	public void setState (long stateA, long stateB, long stateC) {
+		this.stateA = stateA;
+		this.stateB = stateB | 1L;
+		this.stateC = stateC | 1L;
+	}
+
+	@Override
+	public long nextLong () {
+		long x = (stateA += 0xDB4F0B9175AE2165L);
+		x ^= x >>> 32;
+		x *= (stateB += 0xBBE0563303A4615EL);
+		x ^= x >>> 33;
+		x *= (stateC += 0xA0F2EC75A1FE1576L);
+		x ^= x >>> 31;
+		return x;
+	}
+
+	@Override
+	public long previousLong () {
+		long x = (stateA);
+		x ^= x >>> 32;
+		x *= (stateB);
+		x ^= x >>> 33;
+		x *= (stateC);
+		x ^= x >>> 31;
+		stateA -= 0xDB4F0B9175AE2165L;
+		stateB -= 0xBBE0563303A4615EL;
+		stateC -= 0xA0F2EC75A1FE1576L;
+		return x;
+	}
+
+	@Override
+	public long skip(long advance) {
+		long x = (stateA += 0xDB4F0B9175AE2165L * advance);
+		x ^= x >>> 32;
+		x *= (stateB += 0xBBE0563303A4615EL * advance);
+		x ^= x >>> 33;
+		x *= (stateC += 0xA0F2EC75A1FE1576L * advance);
+		x ^= x >>> 31;
+		return x;
+	}
+
+	@Override
+	public int next (int bits) {
+		long x = (stateA += 0xDB4F0B9175AE2165L);
+		x ^= x >>> 32;
+		x *= (stateB += 0xBBE0563303A4615EL);
+		x ^= x >>> 33;
+		x *= (stateC += 0xA0F2EC75A1FE1576L);
+		x ^= x >>> 31;
+		return (int)x >>> (32 - bits);
+	}
+
+
+	@Override
+	public SpoonRandom copy () {
+		return new SpoonRandom(stateA, stateB, stateC);
+	}
+
+	@Override
+	public boolean equals (Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		SpoonRandom that = (SpoonRandom)o;
+
+		return stateA == that.stateA && stateB == that.stateB && stateC == that.stateC;
+	}
+
+	public String toString () {
+		return "SpoonRandom{" + "stateA=" + (stateA) + "L, stateB=" + (stateB) + "L, stateC=" + (stateC) + "L}";
+	}
+
+//	public static void main(String[] args) {
+//		SpoonRandom random = new SpoonRandom(1L);
+//		long n0 = random.nextLong();
+//		long n1 = random.nextLong();
+//		long n2 = random.nextLong();
+//		long n3 = random.nextLong();
+//		long n4 = random.nextLong();
+//		long n5 = random.nextLong();
+//		long n6 = random.nextLong();
+//		long p5 = random.previousLong();
+//		long p4 = random.previousLong();
+//		long p3 = random.previousLong();
+//		long p2 = random.previousLong();
+//		long p1 = random.previousLong();
+//		long p0 = random.previousLong();
+//		System.out.println(n0 == p0);
+//		System.out.println(n1 == p1);
+//		System.out.println(n2 == p2);
+//		System.out.println(n3 == p3);
+//		System.out.println(n4 == p4);
+//		System.out.println(n5 == p5);
+//		System.out.println(n0 + " vs. " + p0);
+//		System.out.println(n1 + " vs. " + p1);
+//		System.out.println(n2 + " vs. " + p2);
+//		System.out.println(n3 + " vs. " + p3);
+//		System.out.println(n4 + " vs. " + p4);
+//		System.out.println(n5 + " vs. " + p5);
+//	}
+}
