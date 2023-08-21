094f9be972981cb10e28026411375d346bfc38ab
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-08-20 21:56:26 -0700

    Add scrambleGaussian(); docs.

--- src/test/java/com/github/tommyettinger/random/gdx/Scramblers.java
+++ src/test/java/com/github/tommyettinger/random/gdx/Scramblers.java
@@ -2,6 +2,22 @@ package com.github.tommyettinger.random.gdx;
 
 import com.badlogic.gdx.utils.NumberUtils;
 
+/**
+ * Static methods that can take any {@code long} as input and produce a very different, but deterministically chosen,
+ * number of some type. The simplest of these conceptually is {@link #scramble(long)}, which takes a long and returns
+ * a different long (well, almost always different; there may be some value that makes scramble() return its input).
+ * You can pass any long to scramble() and can get any long in return. There is also {@link #scrambleBounded(long, int)}
+ * when you want an int outer bound (for this, the inner bound is always 0), {@link #scrambleLong(long, long, long)}
+ * when you want a larger range of bounded values or need to specify arbitrary inner and outer bounds,
+ * {@link #scrambleFloat(long)}/{@link #scrambleDouble(long)} to generate floating-point values between 0 and 1, and
+ * {@link #scrambleGaussian(long)} to get a Gaussian-distributed double (a "bell curve"). Patterns in the input long
+ * values are very difficult to locate if given only the output, and essentially the only way to detect a pattern in
+ * the inputs is to reverse each operation in the specific scramble function and hope you have enough bits to detect any
+ * bias present.
+ * <br>
+ * Each of these uses the MX3 unary hash by Jon Maiga, but XORs the input with 0xABC98388FB8FAC03L before using MX3.
+ * <a href="https://github.com/jonmaiga/mx3">MX3 was provided here</a> and is public domain.
+ */
 public final class Scramblers {
     /**
      * No need to instantiate.
@@ -119,5 +135,30 @@ public final class Scramblers {
         x ^= x >>> 32;
         x *= 0xBEA225F9EB34556DL;
         return (x >>> 11 ^ x >>> 40) * 0x1p-53;
+    }
+
+    /**
+     * Given a long {@code x}, this randomly scrambles x to get a pseudo-random double with a Gaussian distribution
+     * (a "bell curve" distribution centered on 0.0 with standard deviation 1.0). This can return double values
+     * between -9.155293773112453 and 8.209536145151493, both inclusive. The vast majority of results will be closer
+     * to 0.0 than to either of the extreme limits.
+     * <br>
+     * This uses the MX3 unary hash by Jon Maiga, but XORs the input with 0xABC98388FB8FAC03L before using MX3.
+     * It also uses {@link GdxRandom#probit(double)} to distribute the output, and internally uses
+     * {@link GdxRandom#nextExclusiveDouble()}'s small code to prepare an input for probit().
+     * @param x any long
+     * @return a Gaussian-distributed double between -9.155293773112453 and 8.209536145151493
+     */
+    public static double scrambleGaussian(long x) {
+        x ^= 0xABC98388FB8FAC03L;
+        x ^= x >>> 32;
+        x *= 0xBEA225F9EB34556DL;
+        x ^= x >>> 29;
+        x *= 0xBEA225F9EB34556DL;
+        x ^= x >>> 32;
+        x *= 0xBEA225F9EB34556DL;
+        x ^= x >>> 29;
+        return GdxRandom.probit(NumberUtils.longBitsToDouble(1022L - Long.numberOfTrailingZeros(x) << 52 | x >>> 12));
+
     }
 }
