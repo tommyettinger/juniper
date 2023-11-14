d188d71f0340641d46e24a526852e755226d9a0f
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-11-13 22:32:12 -0800

    Try switching between cos approaches.
    In the limited testing, the direct table lookup (Table) was fastest, followed by the interpolated table lookup (Smoother), the non-tabular modified Bhaskara I approximation (Smooth), and Java's Math.cos() (Math). Table offers almost twice the throughput as Math here, even with all approaches calling Math.log1p() for every pixel, which should add a sizable constant factor.

---
+++ src/test/java/com/github/tommyettinger/random/experimental/DctBench.java
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) 2022-2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tommyettinger.random.experimental;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ImmediateModeRenderer20;
+import com.badlogic.gdx.scenes.scene2d.utils.UIUtils;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.github.tommyettinger.digital.ArrayTools;
+import com.github.tommyettinger.digital.BitConversion;
+import com.github.tommyettinger.ds.ObjectList;
+import com.github.tommyettinger.function.ObjSameBiConsumer;
+import com.github.tommyettinger.random.Dct;
+import com.github.tommyettinger.random.Fft;
+
+import java.util.Arrays;
+
+import static com.badlogic.gdx.Input.Keys.*;
+import static com.badlogic.gdx.graphics.GL20.GL_LINES;
+import static com.badlogic.gdx.graphics.GL20.GL_POINTS;
+import static com.github.tommyettinger.random.CorrelationVisualizer.*;
+
+/**
+ */
+public class DctBench extends ApplicationAdapter {
+
+    public static String title = "";
+    public static int dctMode = 0;
+    public static int dctMax = 4;
+    public static ObjectList<ObjSameBiConsumer<double[][]>> transforms =
+            ObjectList.with(DctChoice::transform2DTable, DctChoice::transform2DMath,
+                    DctChoice::transform2DSmooth, DctChoice::transform2DSmoother);
+    public static int randomCount = randoms.length;
+    public static int modeCount = 2;
+    private int currentRandom = 0;
+    private int currentMode = 0;
+    private int selectedBit = 0;
+    private int steps = 0;
+    private Viewport view;
+    private boolean keepGoing = true;
+    private ImmediateModeRenderer20 renderer;
+
+    private static final int width = 256, height = 256;
+    private static final double I255 = 1.0 / 255.0;
+
+    // packed float colors
+    private static final float[][] previousGrid = new float[width << 1][height];
+    private static final float[][] colors = new float[width][height];
+
+    private static final double[][] real = new double[width][height], imag = new double[width][height];
+    private static final int[] freq = new int[256];
+    private static final float LIGHT_YELLOW = Color.toFloatBits(1f, 1f, 0.4f, 1f);
+    private static final float LIGHT_PURPLE = Color.toFloatBits(1f, 0.5f, 1f, 1f);
+
+    public float basicPrepare(int bt)
+    {
+        freq[bt &= 255]++;
+        return BitConversion.intBitsToFloat(0xFE000000 | bt << 16 | bt << 8 | bt);
+    }
+
+    @Override
+    public void create() {
+        Gdx.gl.glDisable(GL20.GL_BLEND);
+        renderer = new ImmediateModeRenderer20(width * height * 2, false, true, 0);
+        view = new ScreenViewport();
+        InputAdapter input = new InputAdapter() {
+            @Override
+            public boolean keyDown(int keycode) {
+                switch (keycode) {
+                    case SPACE:
+                    case P: // pause
+                        keepGoing = !keepGoing;
+                        break;
+                    case S: // step
+                        System.out.println("On step " + steps);
+                        putMap();
+                        break;
+                    case V: // view
+                        System.out.println(randoms[currentRandom][0][0]);
+                        break;
+                    case NUM_1:
+                    case NUMPAD_1:
+                        seedGrid();
+                        steps = 0;
+                        putMap();
+                        break;
+                    case N: // next
+                    case EQUALS:
+                    case ENTER:
+                        currentRandom = ((currentRandom + (UIUtils.shift() ? randomCount - 1 : 1)) % randomCount);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " dct " + dctMode + " mode " + currentMode + " bit " + selectedBit;
+                        System.out.println(title);
+                        steps = 0;
+                        if (!keepGoing) putMap();
+                        break;
+                    case LEFT:
+                        currentRandom = ((currentRandom + randomCount - 1) % randomCount);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " dct " + dctMode + " mode " + currentMode + " bit " + selectedBit;
+                        System.out.println(title);
+                        steps = 0;
+                        if (!keepGoing) putMap();
+                        break;
+                    case RIGHT:
+                        currentRandom = ((currentRandom + 1) % randomCount);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " dct " + dctMode + " mode " + currentMode + " bit " + selectedBit;
+                        System.out.println(title);
+                        steps = 0;
+                        if (!keepGoing) putMap();
+                        break;
+                    case M: // mode
+                        currentMode = ((currentMode + (UIUtils.shift() ? modeCount - 1 : 1)) % modeCount);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " dct " + dctMode + " mode " + currentMode + " bit " + selectedBit;
+                        System.out.println(title);
+                        steps = 0;
+                        if (!keepGoing) putMap();
+                        break;
+                    case UP: // mode
+                        selectedBit = (selectedBit + 1 & 63);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " dct " + dctMode + " mode " + currentMode + " bit " + selectedBit;
+                        System.out.println(title);
+                        steps = 0;
+                        if (!keepGoing) putMap();
+                        break;
+                    case DOWN: // mode
+                        selectedBit = (selectedBit - 1 & 63);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " dct " + dctMode + " mode " + currentMode + " bit " + selectedBit;
+                        System.out.println(title);
+                        steps = 0;
+                        if (!keepGoing) putMap();
+                        break;
+                    case D:
+                        dctMode = ((dctMode + (UIUtils.shift() ? dctMax - 1 : 1)) % dctMax);
+                        refreshGrid();
+                        title = randoms[currentRandom][0][0].getClass().getSimpleName()
+                                + " dct " + dctMode + " mode " + currentMode + " bit " + selectedBit;
+                        System.out.println(title);
+                        steps = 0;
+                        if (!keepGoing) putMap();
+                        break;
+                    case Q: // quit
+                    case ESCAPE: {
+                        Gdx.app.exit();
+                    }
+                }
+                return true;
+            }
+        };
+        Gdx.input.setInputProcessor(input);
+    }
+
+    public void putMap() {
+        ++steps;
+        Arrays.fill(freq, 0);
+        renderer.begin(view.getCamera().combined, GL_POINTS);
+        ArrayTools.fill(imag, 0.0);
+
+        int bt;
+        switch (currentMode) {
+            case 0:
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        bt = (int) randoms[currentRandom][x][y].nextLong() & 255;
+                        real[x][y] = bt * I255;
+                        renderer.color(previousGrid[x][y] = basicPrepare(bt));
+                        renderer.vertex(x, y, 0);
+                    }
+                }
+                break;
+            case 1:
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        bt = (int) (-(randoms[currentRandom][x][y].nextLong() & 1L << selectedBit) >> 63) & 255;
+                        real[x][y] = bt * I255;
+                        renderer.color(previousGrid[x][y] = basicPrepare(bt));
+                        renderer.vertex(x, y, 0);
+                    }
+                }
+                break;
+        }
+
+        transforms.get(dctMode).accept(real, imag);
+        Dct.getColors(real, colors);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                renderer.color(previousGrid[x+width][y] = colors[x][y]);
+                renderer.vertex(x + width, y, 0);
+            }
+        }
+        renderer.end();
+    }
+
+    private void analyze(int[] hist, float packedColor) {
+        renderer.begin(view.getCamera().combined, GL_LINES);
+        for (int i = 0; i < 255; i++) {
+            renderer.color(packedColor);
+            renderer.vertex(i, hist[i] * 0x1p-3f, 0);
+            renderer.color(packedColor);
+            renderer.vertex(i+1, hist[i+1] * 0x1p-3f, 0);
+        }
+        renderer.color(packedColor);
+        renderer.vertex(255, 0, 0);
+        renderer.color(packedColor);
+        renderer.vertex(256, 0, 0);
+        renderer.end();
+
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.BLACK);
+        Gdx.graphics.setTitle(Gdx.graphics.getFramesPerSecond() + " FPS showing " + title);
+        if (keepGoing) {
+            putMap();
+        }
+        else {
+            renderer.begin(view.getCamera().combined, GL_POINTS);
+            for (int x = 0; x < width + width; x++) {
+                for (int y = 0; y < height; y++) {
+                    renderer.color(previousGrid[x][y]);
+                    renderer.vertex(x, y, 0);
+                }
+            }
+            renderer.end();
+        }
+        if(Gdx.input.isKeyPressed(A)){ // Analysis (of random numbers, left side)
+            analyze(freq, LIGHT_YELLOW);
+        }
+        if(Gdx.input.isKeyPressed(H)){ // Histogram (of DFT or DCT, right side)
+            analyze(Fft.histogram, LIGHT_PURPLE);
+        }
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+        view.update(width, height, true);
+        view.apply(true);
+    }
+
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Juniper FFT Visualization");
+        config.setWindowedMode(width << 1, height);
+        config.useVsync(false);
+        config.setForegroundFPS(0);
+        config.setResizable(false);
+        new Lwjgl3Application(new DctBench(), config);
+    }
+}
---
+++ src/test/java/com/github/tommyettinger/random/experimental/DctChoice.java
@@ -0,0 +1,196 @@
+/*
+ * Fast discrete cosine transform algorithms (Java)
+ * 
+ * Copyright (c) 2017 Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/fast-discrete-cosine-transform-algorithms
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+package com.github.tommyettinger.random.experimental;
+
+import com.github.tommyettinger.digital.TrigTools;
+
+/**
+ * Computes the fast discrete cosine transform (DCT-II).
+ * Algorithm by Byeong Gi Lee, 1984. For details, see:
+ * <a href="http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.118.3056&rep=rep1&type=pdf#page=34">this description</a>
+ * of the algorithm, or for the algorithm in its original form, see
+ * <a href="https://www.nayuki.io/res/fast-discrete-cosine-transform-algorithms/lee-new-algo-discrete-cosine-transform.pdf">the original paper</a>.
+ * For the formula, see <a href="https://en.wikipedia.org/wiki/Discrete_cosine_transform#DCT-II">
+ * Wikipedia: Discrete cosine transform - DCT-II</a>.
+ * This implementation comes from <a href="https://www.nayuki.io/page/fast-discrete-cosine-transform-algorithms">Project Nayuki</a>.
+ */
+public final class DctChoice {
+	public static void transformTable(double[] vector, int off, int len, double[] temp) {
+		// Algorithm by Byeong Gi Lee, 1984. For details, see:
+		// http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.118.3056&rep=rep1&type=pdf#page=34
+		if (len == 1)
+			return;
+		int halfLen = len / 2, tInc = TrigTools.TABLE_SIZE / (len + len);
+		for (int i = 0, t = tInc >>> 1; i < halfLen; i++, t += tInc) {
+			double x = vector[off + i];
+			double y = vector[off + len - 1 - i];
+			temp[off + i] = x + y;
+			temp[off + i + halfLen] = (x - y) / (TrigTools.COS_TABLE_D[t] * 2);
+//			temp[off + i + halfLen] = (x - y) / (Math.cos((i + 0.5) * Math.PI / len) * 2);
+		}
+		transformTable(temp, off, halfLen, vector);
+		transformTable(temp, off + halfLen, halfLen, vector);
+		for (int i = 0; i < halfLen - 1; i++) {
+			vector[off + i * 2    ] = temp[off + i];
+			vector[off + i * 2 + 1] = temp[off + i + halfLen] + temp[off + i + halfLen + 1];
+		}
+		vector[off + len - 2] = temp[off + halfLen - 1];
+		vector[off + len - 1] = temp[off + len - 1];
+	}
+
+	public static void transform2DTable(double[][] vector, double[][] temp){
+		final int n = vector.length;
+		for (int x = 0; x < n; x++) {
+			transformTable(vector[x], 0, n, temp[x]);
+		}
+		double swap;
+		for (int x = 0; x < n; x++) {
+			for (int y = x + 1; y < n; y++) {
+				swap = vector[x][y];
+				vector[x][y] = vector[y][x];
+				vector[y][x] = swap;
+			}
+		}
+		for (int x = 0; x < n; x++) {
+			transformTable(vector[x], 0, n, temp[x]);
+		}
+	}
+	public static void transformMath(double[] vector, int off, int len, double[] temp) {
+		// Algorithm by Byeong Gi Lee, 1984. For details, see:
+		// http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.118.3056&rep=rep1&type=pdf#page=34
+		if (len == 1)
+			return;
+		int halfLen = len / 2;
+		for (int i = 0; i < halfLen; i++) {
+			double x = vector[off + i];
+			double y = vector[off + len - 1 - i];
+			temp[off + i] = x + y;
+			temp[off + i + halfLen] = (x - y) / (Math.cos((i + 0.5) * Math.PI / len) * 2);
+		}
+		transformMath(temp, off, halfLen, vector);
+		transformMath(temp, off + halfLen, halfLen, vector);
+		for (int i = 0; i < halfLen - 1; i++) {
+			vector[off + i * 2    ] = temp[off + i];
+			vector[off + i * 2 + 1] = temp[off + i + halfLen] + temp[off + i + halfLen + 1];
+		}
+		vector[off + len - 2] = temp[off + halfLen - 1];
+		vector[off + len - 1] = temp[off + len - 1];
+	}
+
+	public static void transform2DMath(double[][] vector, double[][] temp){
+		final int n = vector.length;
+		for (int x = 0; x < n; x++) {
+			transformMath(vector[x], 0, n, temp[x]);
+		}
+		double swap;
+		for (int x = 0; x < n; x++) {
+			for (int y = x + 1; y < n; y++) {
+				swap = vector[x][y];
+				vector[x][y] = vector[y][x];
+				vector[y][x] = swap;
+			}
+		}
+		for (int x = 0; x < n; x++) {
+			transformMath(vector[x], 0, n, temp[x]);
+		}
+	}
+	public static void transformSmooth(double[] vector, int off, int len, double[] temp) {
+		// Algorithm by Byeong Gi Lee, 1984. For details, see:
+		// http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.118.3056&rep=rep1&type=pdf#page=34
+		if (len == 1)
+			return;
+		int halfLen = len / 2;
+		for (int i = 0; i < halfLen; i++) {
+			double x = vector[off + i];
+			double y = vector[off + len - 1 - i];
+			temp[off + i] = x + y;
+			temp[off + i + halfLen] = (x - y) / (TrigTools.cosSmooth((i + 0.5) * Math.PI / len) * 2);
+		}
+		transformSmooth(temp, off, halfLen, vector);
+		transformSmooth(temp, off + halfLen, halfLen, vector);
+		for (int i = 0; i < halfLen - 1; i++) {
+			vector[off + i * 2    ] = temp[off + i];
+			vector[off + i * 2 + 1] = temp[off + i + halfLen] + temp[off + i + halfLen + 1];
+		}
+		vector[off + len - 2] = temp[off + halfLen - 1];
+		vector[off + len - 1] = temp[off + len - 1];
+	}
+
+	public static void transform2DSmooth(double[][] vector, double[][] temp){
+		final int n = vector.length;
+		for (int x = 0; x < n; x++) {
+			transformSmooth(vector[x], 0, n, temp[x]);
+		}
+		double swap;
+		for (int x = 0; x < n; x++) {
+			for (int y = x + 1; y < n; y++) {
+				swap = vector[x][y];
+				vector[x][y] = vector[y][x];
+				vector[y][x] = swap;
+			}
+		}
+		for (int x = 0; x < n; x++) {
+			transformSmooth(vector[x], 0, n, temp[x]);
+		}
+	}
+	public static void transformSmoother(double[] vector, int off, int len, double[] temp) {
+		// Algorithm by Byeong Gi Lee, 1984. For details, see:
+		// http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.118.3056&rep=rep1&type=pdf#page=34
+		if (len == 1)
+			return;
+		int halfLen = len / 2;
+		for (int i = 0; i < halfLen; i++) {
+			double x = vector[off + i];
+			double y = vector[off + len - 1 - i];
+			temp[off + i] = x + y;
+			temp[off + i + halfLen] = (x - y) / (TrigTools.cosSmoother((i + 0.5) * Math.PI / len) * 2);
+		}
+		transformSmoother(temp, off, halfLen, vector);
+		transformSmoother(temp, off + halfLen, halfLen, vector);
+		for (int i = 0; i < halfLen - 1; i++) {
+			vector[off + i * 2    ] = temp[off + i];
+			vector[off + i * 2 + 1] = temp[off + i + halfLen] + temp[off + i + halfLen + 1];
+		}
+		vector[off + len - 2] = temp[off + halfLen - 1];
+		vector[off + len - 1] = temp[off + len - 1];
+	}
+
+	public static void transform2DSmoother(double[][] vector, double[][] temp){
+		final int n = vector.length;
+		for (int x = 0; x < n; x++) {
+			transformSmoother(vector[x], 0, n, temp[x]);
+		}
+		double swap;
+		for (int x = 0; x < n; x++) {
+			for (int y = x + 1; y < n; y++) {
+				swap = vector[x][y];
+				vector[x][y] = vector[y][x];
+				vector[y][x] = swap;
+			}
+		}
+		for (int x = 0; x < n; x++) {
+			transformSmoother(vector[x], 0, n, temp[x]);
+		}
+	}
+}
