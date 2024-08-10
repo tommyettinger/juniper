316731b4f29357d26c20b2b82a9aa83f4a217f8c
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-08-09 23:18:13 -0700

    Change HoleTest to actually test...
    ... the exclusive float/double methods in EnhancedRandom. This makes what looks like a bug show up! In testExclusiveDouble(), bucket 4 is filled twice as often as it should be, and bucket 9 is filled 4x as often as it should be. Other overfilled buckets are all at low indices. The fix is available and hasn't yet been brought in; it's pretty much just to use trailing zeros instead of leading zeros for the exponent, and to get the mantissa from the upper rather than lower bits.

--- src/test/java/com/github/tommyettinger/random/HoleTest.java
+++ src/test/java/com/github/tommyettinger/random/HoleTest.java
@@ -12,16 +12,12 @@ import org.junit.Test;
 public class HoleTest {
     public static final int PARTITIONS = 1 << 18;
     @Test
-    public void testExclusiveFloat() {
+    public void testInt() {
         IntIntOrderedMap fractionFrequencies = new IntIntOrderedMap(PARTITIONS);
-        for (int i = 0; i < PARTITIONS; i++) {
-            fractionFrequencies.put(i, 0);
-        }
-        GoldenQuasiRandom random = new GoldenQuasiRandom(123L);
+        DistinctRandom random = new DistinctRandom(123L);
         for (int i = 0; i < 0x10000000; i++) {
-            int pos = (int)(random.nextExclusiveFloat() * PARTITIONS);
+            int pos = random.nextInt(PARTITIONS);
             fractionFrequencies.getAndIncrement(pos, 0, 1);
-//            fractionFrequencies.put(pos, fractionFrequencies.get(pos) + 1);
         }
         System.out.println("The least frequent...");
         fractionFrequencies.sortByValue(IntComparators.NATURAL_COMPARATOR);
@@ -33,17 +29,53 @@ public class HoleTest {
             System.out.println(fractionFrequencies.keyAt(i) + ": " + fractionFrequencies.getAt(i));
         }
     }
+
+    @Test
+    public void testLong() {
+        IntIntOrderedMap fractionFrequencies = new IntIntOrderedMap(PARTITIONS);
+        DistinctRandom random = new DistinctRandom(123L);
+        for (int i = 0; i < 0x10000000; i++) {
+            int pos = (int)random.nextLong(PARTITIONS);
+            fractionFrequencies.getAndIncrement(pos, 0, 1);
+        }
+        System.out.println("The least frequent...");
+        fractionFrequencies.sortByValue(IntComparators.NATURAL_COMPARATOR);
+        for (int i = 0; i < 20; i++) {
+            System.out.println(fractionFrequencies.keyAt(i) + ": " + fractionFrequencies.getAt(i));
+        }
+        System.out.println("And the most frequent...");
+        for (int i = fractionFrequencies.size() - 20; i < fractionFrequencies.size(); i++) {
+            System.out.println(fractionFrequencies.keyAt(i) + ": " + fractionFrequencies.getAt(i));
+        }
+    }
+
+    @Test
+    public void testExclusiveFloat() {
+        IntIntOrderedMap fractionFrequencies = new IntIntOrderedMap(PARTITIONS);
+        DistinctRandom random = new DistinctRandom(123L);
+        for (int i = 0; i < 0x10000000; i++) {
+            int pos = (int)(random.nextExclusiveFloat() * PARTITIONS);
+            fractionFrequencies.getAndIncrement(pos, 0, 1);
+        }
+        System.out.println("The least frequent...");
+        fractionFrequencies.sortByValue(IntComparators.NATURAL_COMPARATOR);
+        for (int i = 0; i < 20; i++) {
+            System.out.println(fractionFrequencies.keyAt(i) + ": " + fractionFrequencies.getAt(i));
+        }
+        System.out.println("And the most frequent...");
+        for (int i = fractionFrequencies.size() - 20; i < fractionFrequencies.size(); i++) {
+            System.out.println(fractionFrequencies.keyAt(i) + ": " + fractionFrequencies.getAt(i));
+        }
+    }
+
+
     @Test
     public void testExclusiveDouble() {
         IntIntOrderedMap fractionFrequencies = new IntIntOrderedMap(PARTITIONS);
-        for (int i = 0; i < PARTITIONS; i++) {
-            fractionFrequencies.put(i, 0);
-        }
-        GoldenQuasiRandom random = new GoldenQuasiRandom(123L);
+        DistinctRandom random = new DistinctRandom(123L);
         for (int i = 0; i < 0x10000000; i++) {
-            int pos = (int)(random.nextExclusiveDouble() * PARTITIONS);
+            int pos = (int)(nextExclusiveDoubleOld(random) * PARTITIONS);
             fractionFrequencies.getAndIncrement(pos, 0, 1);
-//            fractionFrequencies.put(pos, fractionFrequencies.get(pos) + 1);
         }
         System.out.println("The least frequent...");
         fractionFrequencies.sortByValue(IntComparators.NATURAL_COMPARATOR);
@@ -63,14 +95,10 @@ public class HoleTest {
     @Test
     public void testExclusiveFloatNew() {
         IntIntOrderedMap fractionFrequencies = new IntIntOrderedMap(PARTITIONS);
-        for (int i = 0; i < PARTITIONS; i++) {
-            fractionFrequencies.put(i, 0);
-        }
-        GoldenQuasiRandom random = new GoldenQuasiRandom(123L);
+        DistinctRandom random = new DistinctRandom(123L);
         for (int i = 0; i < 0x10000000; i++) {
             int pos = (int)(nextExclusiveFloatNew(random) * PARTITIONS);
             fractionFrequencies.getAndIncrement(pos, 0, 1);
-//            fractionFrequencies.put(pos, fractionFrequencies.get(pos) + 1);
         }
         System.out.println("The least frequent...");
         fractionFrequencies.sortByValue(IntComparators.NATURAL_COMPARATOR);
@@ -90,14 +118,10 @@ public class HoleTest {
     @Test
     public void testExclusiveDoubleNew() {
         IntIntOrderedMap fractionFrequencies = new IntIntOrderedMap(PARTITIONS);
-        for (int i = 0; i < PARTITIONS; i++) {
-            fractionFrequencies.put(i, 0);
-        }
-        GoldenQuasiRandom random = new GoldenQuasiRandom(123L);
+        DistinctRandom random = new DistinctRandom(123L);
         for (int i = 0; i < 0x10000000; i++) {
             int pos = (int)(nextExclusiveDoubleNew(random) * PARTITIONS);
             fractionFrequencies.getAndIncrement(pos, 0, 1);
-//            fractionFrequencies.put(pos, fractionFrequencies.get(pos) + 1);
         }
         System.out.println("The least frequent...");
         fractionFrequencies.sortByValue(IntComparators.NATURAL_COMPARATOR);
@@ -112,12 +136,24 @@ public class HoleTest {
 
     private float nextExclusiveFloatNew(EnhancedRandom random) {
         final long bits = random.nextLong();
-        return BitConversion.intBitsToFloat((126 - 63) + BitConversion.countLeadingZeros(1L | BitConversion.lowestOneBit(bits)) << 23 | (int)(bits >>> 41));
+        return BitConversion.intBitsToFloat(126 - BitConversion.countTrailingZeros(bits) << 23 | (int)(bits >>> 41));
+    }
+
+    public double nextExclusiveDoubleOld(EnhancedRandom random) {
+        final long bits = random.nextLong();
+        return BitConversion.longBitsToDouble(1022L - BitConversion.countLeadingZeros(bits) << 52 | (bits & 0xFFFFFFFFFFFFFL));
     }
 
     private double nextExclusiveDoubleNew(EnhancedRandom random) {
         final long bits = random.nextLong();
-        return BitConversion.longBitsToDouble((1022L - 63L) + BitConversion.countLeadingZeros(1L | BitConversion.lowestOneBit(bits)) << 52 | bits >>> 12);
+        return BitConversion.longBitsToDouble((1022L - BitConversion.countTrailingZeros(bits) << 52 | bits >>> 12));
     }
-
+//    private float nextExclusiveFloatNew(EnhancedRandom random) {
+//        final long bits = random.nextLong();
+//        return BitConversion.intBitsToFloat((126 - 63) + BitConversion.countLeadingZeros(1L | BitConversion.lowestOneBit(bits)) << 23 | (int) (bits >>> 41));
+//    }
+//    private double nextExclusiveDoubleNew(EnhancedRandom random) {
+//        final long bits = random.nextLong();
+//        return BitConversion.longBitsToDouble((1022L - 63L) + BitConversion.countLeadingZeros(1L | BitConversion.lowestOneBit(bits)) << 52 | bits >>> 12);
+//    }
 }
