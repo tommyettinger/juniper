a513c3a17cd660397aed6a33eb45fd79b2925db0
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-09-07 03:18:04 -0700

    Add MorbRandom, a single-multiply Orbital.

---
+++ src/test/java/com/github/tommyettinger/random/experimental/MorbRandom.java
@@ -0,0 +1,311 @@
+/*
+ * Copyright (c) 2022-2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.random.experimental;
+
+import com.github.tommyettinger.random.EnhancedRandom;
+
+import java.math.BigInteger;
+
+/**
+ * A two-state generator that updates one state with a large counter and the other with a rarely-updated
+ * counter by 0 or 1. This has a period of exactly 2 to th 128 and all values are allowed for both states.
+ * It has one stream.
+ * <br>
+ * Has passed 64TB of PractRand without anomalies. This is 1D equidistributed, exactly.
+ * <br>
+ * This is related to {@link com.github.tommyettinger.random.OrbitalRandom} in some ways, but uses lighter
+ * mixing and still passes tests.
+ * <br>
+ * Morbillions sold!
+ */
+public class MorbRandom extends EnhancedRandom {
+
+	/**
+	 * The first state; can be any long.
+	 */
+	protected long stateA;
+	/**
+	 * The second state; can be any long.
+	 */
+	protected long stateB;
+
+	/**
+	 * Creates a new MorbRandom with a random state.
+	 */
+	public MorbRandom() {
+		super();
+		stateA = EnhancedRandom.seedFromMath();
+		stateB = EnhancedRandom.seedFromMath();
+	}
+
+	/**
+	 * Creates a new MorbRandom with the given seed; all {@code long} values are permitted.
+	 * The seed will be passed to {@link #setSeed(long)} to attempt to adequately distribute the seed randomly.
+	 *
+	 * @param seed any {@code long} value
+	 */
+	public MorbRandom(long seed) {
+		super(seed);
+		setSeed(seed);
+	}
+
+	/**
+	 * Creates a new MorbRandom with the given two states; all {@code long} values are permitted for
+	 * stateA and for stateB. These states are not changed during assignment.
+	 *
+	 * @param stateA any {@code long} value
+	 * @param stateB any {@code long} value
+	 */
+	public MorbRandom(long stateA, long stateB) {
+		super(stateA);
+		this.stateA = stateA;
+		this.stateB = stateB;
+	}
+
+	@Override
+	public String getTag() {
+		return "MrbR";
+	}
+
+	/**
+	 * Returned by {@link #getMinimumPeriod()}.
+	 * @see #getMinimumPeriod()
+	 */
+	private static final BigInteger MINIMUM_PERIOD = new BigInteger("100000000000000000000000000000000", 16);
+
+	/**
+	 * 2 to the 128.
+	 * @return 2 to the 128
+	 */
+	@Override
+	public BigInteger getMinimumPeriod() {
+		return MINIMUM_PERIOD;
+	}
+
+	/**
+	 * This generator has 2 {@code long} states, so this returns 2.
+	 *
+	 * @return 2 (two)
+	 */
+	@Override
+	public int getStateCount () {
+		return 2;
+	}
+
+	/**
+	 * Gets the state determined by {@code selection}, as-is.
+	 * Selections 0 (or any even number) and 1 (or any odd number) refer to states A and B.
+	 *
+	 * @param selection used to select which state variable to get; generally 0 or 1
+	 * @return the value of the selected state
+	 */
+	@Override
+	public long getSelectedState (int selection) {
+		if ((selection & 1) == 1) {
+			return stateB;
+		}
+		return stateA;
+	}
+
+	/**
+	 * Sets one of the states, determined by {@code selection}, to {@code value}, as-is.
+	 * Selections 0 (or any even number) and 1 (or any odd number) refer to states A and B.
+	 *
+	 * @param selection used to select which state variable to set; generally 0 or 1
+	 * @param value     the exact value to use for the selected state, if valid
+	 */
+	@Override
+	public void setSelectedState (int selection, long value) {
+		if ((selection & 1) == 1) {
+			stateB = value;
+		} else {
+			stateA = value;
+		}
+	}
+
+	/**
+	 * This initializes both states of the generator to random values based on the given seed.
+	 * (2 to the 64) possible initial generator states can be produced here.
+	 *
+	 * @param seed the initial seed; may be any long
+	 */
+	@Override
+	public void setSeed (long seed) {
+		// This is based on MX3, but pulls out values and assigns them to states mid-way, sometimes XORing them.
+		seed += 0x9E3779B97F4A7C15L;
+		seed ^= seed >>> 32;
+		seed *= 0xbea225f9eb34556dL;
+		seed ^= seed >>> 29;
+		seed *= 0xbea225f9eb34556dL;
+		stateA = (seed ^ 0xC6BC279692B5C323L);
+		seed ^= seed >>> 32;
+		seed *= 0xbea225f9eb34556dL;
+		seed ^= seed >>> 29;
+		stateB = (seed ^ ~0xC6BC279692B5C323L);
+	}
+
+	public long getStateA () {
+		return stateA;
+	}
+
+	/**
+	 * Sets the first part of the state.
+	 *
+	 * @param stateA can be any long
+	 */
+	public void setStateA (long stateA) {
+		this.stateA = stateA;
+	}
+
+	public long getStateB () {
+		return stateB;
+	}
+
+	/**
+	 * Sets the second part of the state.
+	 *
+	 * @param stateB can be any long
+	 */
+	public void setStateB (long stateB) {
+		this.stateB = stateB;
+	}
+
+	/**
+	 * Sets the state completely to the given two state variables.
+	 * This is the same as calling {@link #setStateA(long)} and {@link #setStateB(long)}
+	 * as a group.
+	 *
+	 * @param stateA the first state; can be any long
+	 * @param stateB the second state; can be any long
+	 */
+	@Override
+	public void setState (long stateA, long stateB) {
+		this.stateA = stateA;
+		this.stateB = stateB;
+	}
+
+	@Override
+	public long nextLong () {
+		long x = stateA, y = stateB;
+		stateA += 0x9E3779B97F4A7C15L;
+		stateB += (x + (x >>> 1)) >>> 63;
+		x = (x ^ (x << 23 | x >>> 64 - 23) ^ (x << 47 | x >>> 64 - 47) ^ y) * 0xF1357AEA2E62A9C5L;
+		x = (x ^ (x << 25 | x >>> 64 - 25) ^ (x << 50 | x >>> 64 - 50) ^ y);
+		return x;
+	}
+
+	@Override
+	public long previousLong () {
+		stateA -= 0x9E3779B97F4A7C15L;
+		stateB -= (stateA + (stateA >>> 1)) >>> 63;
+		long x = stateA, y = stateB;
+		x = (x ^ (x << 23 | x >>> 64 - 23) ^ (x << 47 | x >>> 64 - 47) ^ y) * 0xF1357AEA2E62A9C5L;
+		x = (x ^ (x << 25 | x >>> 64 - 25) ^ (x << 50 | x >>> 64 - 50) ^ y);
+		return x;
+	}
+
+	@Override
+	public int next (int bits) {
+		long x = stateA, y = stateB;
+		stateA += 0x9E3779B97F4A7C15L;
+		stateB += (x + (x >>> 1)) >>> 63;
+		x = (x ^ (x << 23 | x >>> 64 - 23) ^ (x << 47 | x >>> 64 - 47) ^ y) * 0xF1357AEA2E62A9C5L;
+		x = (x ^ (x << 25 | x >>> 64 - 25) ^ (x << 50 | x >>> 64 - 50) ^ y);
+		return (int)x >>> (32 - bits);
+	}
+
+
+	@Override
+	public MorbRandom copy () {
+		return new MorbRandom(stateA, stateB);
+	}
+
+	@Override
+	public boolean equals (Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		MorbRandom that = (MorbRandom)o;
+
+		return stateA == that.stateA && stateB == that.stateB;
+	}
+
+	public String toString () {
+		return "MorbRandom{" + "stateA=" + (stateA) + "L, stateB=" + (stateB) + "L}";
+	}
+
+//	public static void main(String[] args) {
+//		MorbRandom random = new MorbRandom(1L);
+//		{
+//			int n0 = random.nextInt();
+//			int n1 = random.nextInt();
+//			int n2 = random.nextInt();
+//			int n3 = random.nextInt();
+//			int n4 = random.nextInt();
+//			int n5 = random.nextInt();
+//			int p5 = random.previousInt();
+//			int p4 = random.previousInt();
+//			int p3 = random.previousInt();
+//			int p2 = random.previousInt();
+//			int p1 = random.previousInt();
+//			int p0 = random.previousInt();
+//			System.out.println(n0 == p0);
+//			System.out.println(n1 == p1);
+//			System.out.println(n2 == p2);
+//			System.out.println(n3 == p3);
+//			System.out.println(n4 == p4);
+//			System.out.println(n5 == p5);
+//			System.out.println(Base.BASE16.unsigned(n0) + " vs. " + Base.BASE16.unsigned(p0));
+//			System.out.println(Base.BASE16.unsigned(n1) + " vs. " + Base.BASE16.unsigned(p1));
+//			System.out.println(Base.BASE16.unsigned(n2) + " vs. " + Base.BASE16.unsigned(p2));
+//			System.out.println(Base.BASE16.unsigned(n3) + " vs. " + Base.BASE16.unsigned(p3));
+//			System.out.println(Base.BASE16.unsigned(n4) + " vs. " + Base.BASE16.unsigned(p4));
+//			System.out.println(Base.BASE16.unsigned(n5) + " vs. " + Base.BASE16.unsigned(p5));
+//		}
+//		random = new MorbRandom(1L);
+//		{
+//			long n0 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long n1 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long n2 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long n3 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long n4 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long n5 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			System.out.println("Going back...");
+//			long p5 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long p4 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long p3 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long p2 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long p1 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			long p0 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+//			System.out.println(n0 == p0);
+//			System.out.println(n1 == p1);
+//			System.out.println(n2 == p2);
+//			System.out.println(n3 == p3);
+//			System.out.println(n4 == p4);
+//			System.out.println(n5 == p5);
+//			System.out.println(Base.BASE16.unsigned(n0) + " vs. " + Base.BASE16.unsigned(p0));
+//			System.out.println(Base.BASE16.unsigned(n1) + " vs. " + Base.BASE16.unsigned(p1));
+//			System.out.println(Base.BASE16.unsigned(n2) + " vs. " + Base.BASE16.unsigned(p2));
+//			System.out.println(Base.BASE16.unsigned(n3) + " vs. " + Base.BASE16.unsigned(p3));
+//			System.out.println(Base.BASE16.unsigned(n4) + " vs. " + Base.BASE16.unsigned(p4));
+//			System.out.println(Base.BASE16.unsigned(n5) + " vs. " + Base.BASE16.unsigned(p5));
+//		}
+//	}
+}
