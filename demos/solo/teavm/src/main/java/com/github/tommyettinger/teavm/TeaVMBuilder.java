6ed2ac4a15e5f9b7889bf7590a92b184c0b2b83b
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-01-14 01:27:32 -0800

    And finally Solo demo update.

--- demos/solo/teavm/src/main/java/com/github/tommyettinger/teavm/TeaVMBuilder.java
+++ demos/solo/teavm/src/main/java/com/github/tommyettinger/teavm/TeaVMBuilder.java
@@ -36,8 +36,8 @@ public class TeaVMBuilder {
 		// Register any classes or packages that require reflection here:
 		// TeaReflectionSupplier.addReflectionClass("com.github.tommyettinger.reflect");
 
-		teaBuildConfiguration.targetType = TeaVMTargetType.JAVASCRIPT;
-//		teaBuildConfiguration.targetType = TeaVMTargetType.WEBASSEMBLY_GC;
+//		teaBuildConfiguration.targetType = TeaVMTargetType.JAVASCRIPT;
+		teaBuildConfiguration.targetType = TeaVMTargetType.WEBASSEMBLY_GC;
 
 		TeaBuilder.config(teaBuildConfiguration);
 		TeaVMTool tool = new TeaVMTool();
