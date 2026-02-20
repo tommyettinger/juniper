b4af6b8382da127f2b8e0e612cc46498c24a2b45
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-19 19:35:59 -0800

    Start on ser/deser; this really needs testing.

--- src/test/java/com/github/tommyettinger/random/experimental/CompositeWrapper.java
+++ src/test/java/com/github/tommyettinger/random/experimental/CompositeWrapper.java
@@ -1,9 +1,9 @@
 package com.github.tommyettinger.random.experimental;
 
-import com.github.tommyettinger.random.EnhancedRandom;
-import com.github.tommyettinger.random.OrbitalRandom;
-import com.github.tommyettinger.random.Xoshiro256StarStarRandom;
+import com.github.tommyettinger.digital.Base;
+import com.github.tommyettinger.random.*;
 
+import java.io.IOException;
 import java.math.BigInteger;
 
 public class CompositeWrapper extends EnhancedRandom {
@@ -104,7 +104,7 @@ public class CompositeWrapper extends EnhancedRandom {
 	}
 
 	public void setRandomA(EnhancedRandom randomA) {
-		if(randomA != null) this.randomA = randomA;
+		if (randomA != null) this.randomA = randomA;
 	}
 
 	public EnhancedRandom getRandomB() {
@@ -112,7 +112,7 @@ public class CompositeWrapper extends EnhancedRandom {
 	}
 
 	public void setRandomB(EnhancedRandom randomB) {
-		if(randomB != null) this.randomB = randomB;
+		if (randomB != null) this.randomB = randomB;
 	}
 
 	@Override
@@ -134,5 +134,60 @@ public class CompositeWrapper extends EnhancedRandom {
 			"randomA=" + randomA +
 			", randomB=" + randomB +
 			'}';
+	}
+
+	/**
+	 * Serializes the current state of this EnhancedRandom to a String that can be used by
+	 * {@link #stringDeserialize(String)} to load this state at another time.
+	 *
+	 * @param base which Base to use, from the "digital" library, such as {@link Base#BASE10}
+	 * @return a String storing all data from the EnhancedRandom part of this generator
+	 */
+	@Override
+	public String stringSerialize(Base base) {
+		String a = randomA.stringSerialize(base);
+		String b = randomB.stringSerialize(base);
+		return getTag() + base.paddingChar
+			+ base.signed(a.length()) + base.paddingChar + a
+			+ base.signed(b.length()) + base.paddingChar + b;
+	}
+
+	@Override
+	public <T extends CharSequence & Appendable> T appendSerialized(T sb, Base base) {
+		try {
+			String a = randomA.stringSerialize(base);
+			String b = randomB.stringSerialize(base);
+			sb.append(getTag());
+			sb.append(base.paddingChar);
+			base.appendSigned(sb, a.length());
+			sb.append(base.paddingChar);
+			sb.append(a);
+			base.appendSigned(sb, b.length());
+			sb.append(base.paddingChar);
+			sb.append(b);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return sb;
+	}
+
+	/**
+	 * Given a String in the format produced by {@link #stringSerialize(Base)}, and the same {@link Base} used by
+	 * the serialization, this will attempt to set this EnhancedRandom object to match the state in the serialized
+	 * data. This only works if this EnhancedRandom is the same implementation that was serialized, and also needs
+	 * the Bases to be identical. Returns this EnhancedRandom, after possibly changing its state.
+	 *
+	 * @param data a String probably produced by {@link #stringSerialize(Base)}
+	 * @param base which Base to use, from the "digital" library, such as {@link Base#BASE10}
+	 * @return this, after setting its state
+	 */
+	@Override
+	public CompositeWrapper stringDeserialize(String data, Base base) {
+		int start = data.indexOf(base.paddingChar) + 1;
+		int len = base.readInt(data, start, start = data.indexOf(base.paddingChar, start));
+		setRandomA(Deserializer.deserialize(data.substring(start + 1, start += len), base));
+		len = base.readInt(data, start + 1, start = data.indexOf(base.paddingChar, start + 1));
+		setRandomB(Deserializer.deserialize(data.substring(start + 1, start + len), base));
+		return this;
 	}
 }
