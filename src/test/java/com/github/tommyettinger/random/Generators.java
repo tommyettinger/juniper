dee306b61277ae57e08254c40cc5e057f25eb8c3
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-14 01:54:23 -0700

    Quiz shows serious correlation.

--- src/test/java/com/github/tommyettinger/random/Generators.java
+++ src/test/java/com/github/tommyettinger/random/Generators.java
@@ -126,6 +126,7 @@ public final class Generators {
 		randomList.add(new Lambeau32Random(1, 1, 1));
 		randomList.add(new GulfRandom(1, 1));
 		randomList.add(new GolfRandom(1, 1));
+		randomList.add(new QuizRandom(1, 1));
 	}
 
 	public static int randomCount = randomList.size();
