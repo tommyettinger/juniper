a7472b2e30f9c6a74310e4f209159952947b37fa
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-24 00:12:28 -0700

    This one passed PractRand; might as well add it!

---
+++ src/test/java/com/github/tommyettinger/random/experimental/SizzleRandom.java
@@ -0,0 +1,314 @@
+/*
+ * Copyright (c) 2022-2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.random.experimental;
+
+import com.github.tommyettinger.digital.BitConversion;
+import com.github.tommyettinger.random.EnhancedRandom;
+
+import java.math.BigInteger;
+
+/**
+ * A 64-bit generator that uses just one multiplication per result, and lots of bitwise rotation. It uses two counters
+ * and has a period of 2 to the 128. It is perfectly 1D-equidistributed. It passes 128TB of PractRand with no anomalies.
+ * <br>
+ * The name is because it involves count-leading-zeros (Z) operations and seems to adequately "cook" the inputs.
+ */
+public class SizzleRandom extends EnhancedRandom {
+
+	/**
+	 * The first state; can be any long.
+	 */
+	protected long stateA;
+	/**
+	 * The second state; can be any long.
+	 */
+	protected long stateB;
+
+	/**
+	 * Creates a new SizzleRandom with a random state.
+	 */
+	public SizzleRandom() {
+		super();
+		stateA = EnhancedRandom.seedFromMath();
+		stateB = EnhancedRandom.seedFromMath();
+	}
+
+	/**
+	 * Creates a new SizzleRandom with the given seed; all {@code long} values are permitted.
+	 * The seed will be passed to {@link #setSeed(long)} to attempt to adequately distribute the seed randomly.
+	 *
+	 * @param seed any {@code long} value
+	 */
+	public SizzleRandom(long seed) {
+		super(seed);
+		setSeed(seed);
+	}
+
+	/**
+	 * Creates a new SizzleRandom with the given two states; all {@code long} values are permitted for
+	 * stateA and for stateB. These states are not changed.
+	 *
+	 * @param stateA any {@code long} value
+	 * @param stateB any {@code long} value
+	 */
+	public SizzleRandom(long stateA, long stateB) {
+		super(stateA);
+		this.stateA = stateA;
+		this.stateB = stateB;
+	}
+
+	@Override
+	public String getTag() {
+		return "SzzR";
+	}
+
+	/**
+	 * Returned by {@link #getMinimumPeriod()}.
+	 *
+	 * @see #getMinimumPeriod()
+	 */
+	private static final BigInteger MINIMUM_PERIOD = new BigInteger("100000000000000000000000000000000", 16);
+
+	/**
+	 * 2 to the 128.
+	 *
+	 * @return 2 to the 128
+	 */
+	@Override
+	public BigInteger getMinimumPeriod() {
+		return MINIMUM_PERIOD;
+	}
+
+	/**
+	 * This generator has 2 {@code long} states, so this returns 2.
+	 *
+	 * @return 2 (two)
+	 */
+	@Override
+	public int getStateCount() {
+		return 2;
+	}
+
+	/**
+	 * Gets the state determined by {@code selection}, as-is.
+	 * Selections 0 (or any even number) and 1 (or any odd number) refer to states A and B.
+	 *
+	 * @param selection used to select which state variable to get; generally 0 or 1
+	 * @return the value of the selected state
+	 */
+	@Override
+	public long getSelectedState(int selection) {
+		if ((selection & 1) == 1) {
+			return stateB;
+		}
+		return stateA;
+	}
+
+	/**
+	 * Sets one of the states, determined by {@code selection}, to {@code value}, as-is.
+	 * Selections 0 (or any even number) and 1 (or any odd number) refer to states A and B.
+	 *
+	 * @param selection used to select which state variable to set; generally 0 or 1
+	 * @param value     the value to use for the selected state; used verbatim
+	 */
+	@Override
+	public void setSelectedState(int selection, long value) {
+		if ((selection & 1) == 1) {
+			stateB = value;
+		} else {
+			stateA = value;
+		}
+	}
+
+	/**
+	 * This initializes both states of the generator to random values based on the given seed.
+	 * (2 to the 64) possible initial generator states can be produced here.
+	 *
+	 * @param seed the initial seed; may be any long
+	 */
+	@Override
+	public void setSeed(long seed) {
+		long x = (seed + 0x9E3779B97F4A7C15L);
+		x ^= x >>> 27;
+		x *= 0x3C79AC492BA7B653L;
+		x ^= x >>> 33;
+		x *= 0x1C69B3F74AC4AE35L;
+		stateA = x ^= x >>> 27;
+		x += 0x9E3779B97F4A7C15L;
+		x ^= x >>> 27;
+		x *= 0x3C79AC492BA7B653L;
+		x ^= x >>> 33;
+		x *= 0x1C69B3F74AC4AE35L;
+		stateB = x ^ x >>> 27;
+	}
+
+	public long getStateA() {
+		return stateA;
+	}
+
+	/**
+	 * Sets the first part of the state.
+	 *
+	 * @param stateA can be any long
+	 */
+	public void setStateA(long stateA) {
+		this.stateA = stateA;
+	}
+
+	public long getStateB() {
+		return stateB;
+	}
+
+	/**
+	 * Sets the second part of the state.
+	 *
+	 * @param stateB can be any long
+	 */
+	public void setStateB(long stateB) {
+		this.stateB = stateB;
+	}
+
+	/**
+	 * Sets the state completely to the given two state variables.
+	 * This is the same as calling {@link #setStateA(long)} and {@link #setStateB(long)}
+	 * as a group.
+	 *
+	 * @param stateA the first state; can be any long
+	 * @param stateB the second state; can be any long
+	 */
+	@Override
+	public void setState(long stateA, long stateB) {
+		this.stateA = stateA;
+		this.stateB = stateB;
+	}
+
+	@Override
+	public long nextLong() {
+		long x = stateA ^ stateB;
+		x ^= (x << 11 | x >>> 53) ^ (x << 41 | x >>> 23);
+		x *= 0xF1357AEA2E62A9C5L;
+		x ^= (x << 25 | x >>> 39) ^ (x << 50 | x >>> 14);
+		stateB += BitConversion.countLeadingZeros(stateA);
+		stateA += 0xD1342543DE82EF95L;
+		return x;
+	}
+
+	@Override
+	public long previousLong() {
+		stateA -= 0xD1342543DE82EF95L;
+		stateB -= BitConversion.countLeadingZeros(stateA);
+		long x = stateA ^ stateB;
+		x ^= (x << 11 | x >>> 53) ^ (x << 41 | x >>> 23);
+		x *= 0xF1357AEA2E62A9C5L;
+		x ^= (x << 25 | x >>> 39) ^ (x << 50 | x >>> 14);
+		return x;
+
+	}
+
+	@Override
+	public int next(int bits) {
+		long x = stateA ^ stateB;
+		x ^= (x << 11 | x >>> 53) ^ (x << 41 | x >>> 23);
+		x *= 0xF1357AEA2E62A9C5L;
+		x ^= (x << 25 | x >>> 39) ^ (x << 50 | x >>> 14);
+		stateB += BitConversion.countLeadingZeros(stateA);
+		stateA += 0xD1342543DE82EF95L;
+		return (int) x >>> (32 - bits);
+	}
+
+	@Override
+	public SizzleRandom copy() {
+		return new SizzleRandom(stateA, stateB);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+
+		SizzleRandom that = (SizzleRandom) o;
+
+		if (stateA != that.stateA)
+			return false;
+		return stateB == that.stateB;
+	}
+
+	public String toString() {
+		return "SizzleRandom{" + "stateA=" + (stateA) + "L, stateB=" + (stateB) + "L}";
+	}
+
+	public static void main(String[] args) {
+		SizzleRandom random = new SizzleRandom(1L);
+		{
+			int n0 = random.nextInt();
+			int n1 = random.nextInt();
+			int n2 = random.nextInt();
+			int n3 = random.nextInt();
+			int n4 = random.nextInt();
+			int n5 = random.nextInt();
+			int p5 = random.previousInt();
+			int p4 = random.previousInt();
+			int p3 = random.previousInt();
+			int p2 = random.previousInt();
+			int p1 = random.previousInt();
+			int p0 = random.previousInt();
+			System.out.println(n0 == p0);
+			System.out.println(n1 == p1);
+			System.out.println(n2 == p2);
+			System.out.println(n3 == p3);
+			System.out.println(n4 == p4);
+			System.out.println(n5 == p5);
+			System.out.printf("%08X vs. %08X\n", p0, n0);
+			System.out.printf("%08X vs. %08X\n", p1, n1);
+			System.out.printf("%08X vs. %08X\n", p2, n2);
+			System.out.printf("%08X vs. %08X\n", p3, n3);
+			System.out.printf("%08X vs. %08X\n", p4, n4);
+			System.out.printf("%08X vs. %08X\n", p5, n5);
+		}
+		random.setSeed(1L);
+		{
+			long n0 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long n1 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long n2 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long n3 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long n4 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long n5 = random.nextLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			System.out.println("Going back...");
+			long p5 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long p4 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long p3 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long p2 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long p1 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			long p0 = random.previousLong(); System.out.printf("a: 0x%016XL, b: 0x%016XL\n", random.stateA, random.stateB);
+			System.out.println(n0 == p0);
+			System.out.println(n1 == p1);
+			System.out.println(n2 == p2);
+			System.out.println(n3 == p3);
+			System.out.println(n4 == p4);
+			System.out.println(n5 == p5);
+			System.out.printf("%016X vs. %016X\n", n0, p0);
+			System.out.printf("%016X vs. %016X\n", n1, p1);
+			System.out.printf("%016X vs. %016X\n", n2, p2);
+			System.out.printf("%016X vs. %016X\n", n3, p3);
+			System.out.printf("%016X vs. %016X\n", n4, p4);
+			System.out.printf("%016X vs. %016X\n", n5, p5);
+		}
+	}
+}
