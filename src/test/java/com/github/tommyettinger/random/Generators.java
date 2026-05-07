28544c141ee68236548eacb12f92f875564c9a3c
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-05-07 00:04:21 -0700

    Flamberge does significantly WORSE in ICE tests!

--- src/test/java/com/github/tommyettinger/random/Generators.java
+++ src/test/java/com/github/tommyettinger/random/Generators.java
@@ -129,6 +129,7 @@ public final class Generators {
 		randomList.add(new GolfRandom(1, 1));
 		randomList.add(new SizzleRandom(1, 1));
 		randomList.add(new FlamencoRandom(1, 1));
+		randomList.add(new FlambergeRandom(1, 1));
 	}
 
 	public static int randomCount = randomList.size();
