9cc276e366ac7bf4c05a049a5c32aee3ba407295
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-07-14 21:46:57 -0700

    Well, it's bad!

--- src/test/java/com/github/tommyettinger/random/Generators.java
+++ src/test/java/com/github/tommyettinger/random/Generators.java
@@ -144,6 +144,7 @@ public final class Generators {
 		randomList.add(new WoolRandom(1));
 		randomList.add(new YarnRandom(1));
 		randomList.add(new CompositeWrapper(new LFSR64QuasiRandom(1), new QoaxsRandom(1)));
+		randomList.add(new TriQoaRandom(1, 1, 1));
 	}
 
 	public static int randomCount = randomList.size();
