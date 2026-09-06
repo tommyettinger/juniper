5def0c4f8937c70fd48f223d841d72d6100ba7f1
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-09-05 20:16:25 -0700

    Goblin3 and Goblin4 fail ICE/IICE tests (for now).

--- src/test/java/com/github/tommyettinger/random/Generators.java
+++ src/test/java/com/github/tommyettinger/random/Generators.java
@@ -153,6 +153,8 @@ public final class Generators {
 		randomList.add(new GyozaRandom(1, 1));
 		randomList.add(new LowGyo1Random(1, 1));
 		randomList.add(new LowGyo2Random(1, 1));
+		randomList.add(new Goblin3Random(1, 1, 1));
+		randomList.add(new Goblin4Random(1, 1, 1, 1));
 	}
 
 	public static int randomCount = randomList.size();
